9ff1893c9c391df4f11c3fa10b8c66c9a22b20ad
suixi.hong <suixi.hong@chukou1.com>
2018-01-30 23:04:01 +0800

    update：init

---
+++ MySSOTest/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MySSOTest
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
